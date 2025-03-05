800b0951a7775565769e9ed7bc2b97a34a52917c
appinha <amanda_pinha@hotmail.com>
2025-03-05 10:02:36 -0300

    personalize footer

---
+++ src/constants/socials.ts
@@ -0,0 +1,6 @@
+import type { iconPaths } from "../components/IconPaths";
+
+export const socials: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
+	{ label: 'LinkedIn', href: 'https://www.linkedin.com/in/appinha/', icon: 'linkedin-logo' },
+	{ label: 'GitHub', href: 'https://github.com/appinha', icon: 'github-logo' },
+];
