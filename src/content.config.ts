86a613b6e65abeb9c76d38d71eeae28d2779a16d
appinha <amanda_pinha@hotmail.com>
2025-03-11 13:27:18 -0300

    format files

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,10 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
 	blog: defineCollection({
 		// Load Markdown files in the src/content/blog directory.
-		loader: glob({ base: './src/content/blog', pattern: '**/*.md', }),
+		loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
